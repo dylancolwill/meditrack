91bba595614ca9a84726dd1e585b122fe11d0cb0
dylancolwill <dylan.colwill@hotmail.com>
2025-04-22 22:49:30 +1000

    add patient page

---
+++ src/js/newPatient.js
@@ -0,0 +1,6 @@
+function savePatient() {
+    const firstName = document.getElementById('firstName').value;
+    const lastName = document.getElementById('lastName').value;
+    const patientAddress = document.getElementById('patientAddress').value;
+    const patientHeathcareProvider = document.getElementById('patientHeathcareProvider').value;
+}
