53b33a0f226825b15e320308f5bbb2fba2970f9c
dylancolwill <dylan.colwill@hotmail.com>
2025-04-22 14:10:49 +1000

    add patient info functionality

--- src/js/addPatientInfoScript.js
+++ src/js/addPatientInfoScript.js
@@ -1,69 +1,27 @@
 changeEditTarget("allergy") //default, find better way
 
-function onSelect() {
+document.addEventListener('DOMContentLoaded', () => {
+    const PATIENT_ID_TO_DISPLAY = 'P001';
+
+    //get data
+    const data = getPatientData(PATIENT_ID_TO_DISPLAY);
+});
+
+function onSelect(event) {
     catagory = document.getElementById("catagories").value;
 
-    changeEditTarget(catagory)
+    changeEditTarget(event)
 }
 
-//change for aws data retrieval
-function getPatientData(patientId) {
-    const patient = db.patients.find(p => p.patientId === patientId);
-    if (!patient) return null;
+function changeEditTarget(evt) {
+    cityName = document.getElementById("catagories").value;
 
-    //find associated data
-    const episodes = db.episodes.filter(e => e.patientId === patientId);
-    const medications = db.medications.filter(m => m.patientId === patientId);
-    const conditions = db.conditions.filter(c => c.patientId === patientId);
-    const adverseReactions = db.adverseReactions.filter(ar => ar.patientId === patientId);
-    //clinical data associated with patient episodes
-    const episodeIds = episodes.map(ep => ep.episodeId);
-    const clinicalData = db.clinicalData.filter(cd => episodeIds.includes(cd.episodeId));
-    const vaccinations = db.vaccinations.filter(v => v.patientId === patientId)
 
-    return {
-        patient,
-        episodes,
-        medications,
-        conditions,
-        adverseReactions,
-        medicalStaff: db.medicalStaff,
-        clinicalData,
-        vaccinations
-    };
-}
-
-function changeEditTarget(catagory) {
-    var allergyTable = document.getElementById("allergy-table");
-    var conditionTable = document.getElementById("condition-table");
-    var medicationTable = document.getElementById("medication-table");
-    var vaccinationTable = document.getElementById("vaccination-table");
-    
-
-    document.getElementById("catagory-title").innerHTML = catagory;
-
-    if (catagory=="allergy") {
-        allergyTable.style.display = "block";
-        conditionTable.style.display = "none";
-        medicationTable.style.display = "none";
-        vaccinationTable.style.display = "none";
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("catagories-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
     }
-    else if (catagory=="condition"){
-        conditionTable.style.display = "block";
-        allergyTable.style.display = "none";
-        medicationTable.style.display = "none";
-        vaccinationTable.style.display = "none";
-    }
-    else if (catagory=="medication"){
-        medicationTable.style.display = "block";
-        allergyTable.style.display = "none";
-        conditionTable.style.display = "none";
-        vaccinationTable.style.display = "none";
-    }
-    else if (catagory=="vaccination"){
-        vaccinationTable.style.display = "block";
-        allergyTable.style.display = "none";
-        conditionTable.style.display = "none";
-        medicationTable.style.display = "none";
-    }
+
+    document.getElementById(cityName).style.display = "block";
 }
