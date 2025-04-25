a98e3ea148c4e774b81b141472624930da813019
unknown <perrylfc@gmail.com>
2025-04-25 21:22:57 +1000

    Hope this works

---
+++ src/php/connectDisplay.php
@@ -0,0 +1,44 @@
+<?php
+ini_set('display_errors', 1); // Turn on error displaying
+error_reporting(E_ALL);     // Report all PHP errors
+
+// Database connection parameters
+$db_host = 'database-1.cxomy0mse0pi.ap-southeast-2.rds.amazonaws.com';
+$db_user = 'admin';
+$db_pass = 'admin123!';
+$db_name = 'medical2';
+$db_port = '3306';
+
+$link = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
+
+
+if ($link->connect_error) {
+    die("Connection failed: (" . $link->connect_errno . ") " . $link->connect_error);
+}
+
+echo "connected<br>";
+
+
+$sql = "SELECT fname FROM patient";
+echo "work";
+$result = $link->query($sql);
+echo "work2";
+if ($result) {
+    if ($result->num_rows > 0) {
+        echo "fnames :<br>";
+        while($row = $result->fetch_assoc()) {
+            echo htmlspecialchars($row["fname"]) . "<br>";
+        }
+    } else {
+        echo "nothign found";
+    }
+    $result->free();
+} else {
+    echo "error (" . $link->errno . ") " . $link->error;
+}
+
+// $link->close();
+
+// echo "Connection closed.";
+
+?>
