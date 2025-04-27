3546c6c42faafed522936e944cee9097fc36f0aa
dylancolwill <dylan.colwill@hotmail.com>
2025-04-27 21:31:09 +1000

    testing

--- src/patientInfo.php
+++ src/patientInfo.php
@@ -2,6 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+echo 'patifntinfo page id: '.$patientInfo;
 
 // session_start();
 include 'connectors/queryPatientInfo.php'; ?>
