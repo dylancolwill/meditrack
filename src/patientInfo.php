dc35aeb86d8c679a9799f43c847a94f56a473983
dylancolwill <dylan.colwill@hotmail.com>
2025-04-27 22:03:28 +1000

    fix wrong patient

--- src/patientInfo.php
+++ src/patientInfo.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 // session_start();
 include 'connectors/queryPatientInfo.php'; 
-var_dump($patientData);
+// var_dump($patientData);
 ?>
 <!DOCTYPE html>
 <html lang="en">
